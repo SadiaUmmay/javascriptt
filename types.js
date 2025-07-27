b712f5bd1c68535f6cfb140f5b1c0cb5ba078042
unknown <ummayjannatsadia@gmail.com>
2025-07-27 22:25:09 +0600

    new

---
+++ types.js
@@ -0,0 +1,15 @@
+// String Type----- 
+var name = 'hambu';
+// console.log(name)
+
+var address = "hi what are you doing?"
+// console.log(address)
+
+
+// Boolean type---------
+var isrich = true;
+console.log(isrich)
+
+
+var passed = false;
+console.log(typeof passed)
---
+++ variable.js
@@ -0,0 +1,16 @@
+// 5 things to declare a variable 
+var weight = 38;
+var price = 38;
+var year = 2000;
+var money = 209;
+console.log(year)
+
+
+
+
+
+
+var age =17;
+age = 35;
+
+console.log(age);
