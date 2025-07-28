54490e85282c5bc35206189e5aede3e107bc9a3d
unknown <ummayjannatsadia@gmail.com>
2025-07-29 00:30:33 +0600

    new

--- number.js
+++ number.js
@@ -1,2 +1,18 @@
-var orangePrice = 20;
+var orangePrice = '20';
 var chocolate = 0.5;
+
+// console.log(orangePrice+ chocolate)
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+
+// console.log(total.toFixed(1))
+
+const num = 50/0;
+console.log(num)
+
+var onion = 20;
+var egg = 6;
+var total = (onion/egg)
+console.log(total)
