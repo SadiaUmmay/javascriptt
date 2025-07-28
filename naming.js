a3068324243738fee83741ba3082ad9cd32b38d8
unknown <ummayjannatsadia@gmail.com>
2025-07-28 23:45:31 +0600

    name

---
+++ naming.js
@@ -0,0 +1,29 @@
+// no keyward in variable 
+var false = 30;
+var var =7;
+
+// no space or gap in variable 
+
+var is happy = false;
+var ishappy= true;
+
+// no quote 
+var 'address' = 'kochu khet';
+
+// cannot start with a number . but number other than the first letter is alowed
+
+var money = 55;
+var 3money =33;
+var money3 = 29;
+
+// name is case sensitive 
+
+address
+Address
+ADDRESS
+
+// how to write a long variable name 
+
+var my_current_address =  "jshdjhdjshd" //snake case
+var myCurrentAddress =  "jshdjhdjshd" //camel case
+var MyCurrentAddress =  "jshdjhdjshd" //pascal case
