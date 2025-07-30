7e80153686cacb5b603e05d3c2ed8d64d2864398
unknown <ummayjannatsadia@gmail.com>
2025-07-30 20:57:24 +0600

    tas

---
+++ practice.js
@@ -0,0 +1,29 @@
+// 1 math 
+var mainMoney = 1000;
+var applePrice = 300;
+var orangePrice = 180;
+var totalCost = applePrice + orangePrice;
+var returnTk = mainMoney - totalCost;
+console.log(returnTk)
+
+// 2 math 
+var math = 70;
+var bio = 78;
+var chem = 83;
+var phy = 80;
+var bangla = 88;
+var total = math + bio + chem + phy+ bangla;
+var average = total/5;
+console.log(average.toFixed(2)) 
+
+// 3 math 
+var input = 119;
+var reminder = input% 5;
+console.log(reminder)
+
+// 4 math 
+const a = isNaN('11');
+console.log(a)
+
+const b = isNaN(2-10);
+console.log(b)
