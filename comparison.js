8692f11ae14cec0252ea2a25b44986ff5d32ebfd
unknown <ummayjannatsadia@gmail.com>
2025-07-31 17:42:40 +0600

    14.2

---
+++ comparison.js
@@ -0,0 +1,29 @@
+/*  comparison
+1. bigger: >
+2. less : <
+3. equal: ==
+4. greater than or equal: >=
+5. less than or equal: <=
+6. not equal !=
+7. and: &&
+8. or: ||
+*/
+
+// console.log(5==10);
+// console.log(10==10);
+// console.log(10 === '10');
+// console.log(10 >=10);
+// console.log(15>=10);
+// console.log(10<=10);
+// console.log(10 ==10.1);
+
+
+//..............................
+
+
+var peyaraPrice = 100;
+var applePrice= 240;
+
+// console.log(peyaraPrice> applePrice)
+
+console.log(10 != '10')
