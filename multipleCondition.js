96e4de299b609897a3745935476324c6d76ec50c
unknown <ummayjannatsadia@gmail.com>
2025-07-31 18:12:06 +0600

    14.5

---
+++ multipleCondition.js
@@ -0,0 +1,45 @@
+const salary = 25000;
+const isBCS = true;
+const height = 65;
+const hasCar = true
+// if (salary> 20000 && height>64){
+//     console.log('shu patro');
+// }
+// else{
+//     console.log('onno patro khuji')
+// }
+
+// if (salary> 25000 || height>52){
+//     console.log('esho baba kobul')
+
+// }
+// else{
+//     console.log('vag tui mokbul')
+// }
+
+
+// more condition .................
+
+// if (salary> 25000 || height>72 || isBCS==true){
+//     console.log('esho baba kobul')
+
+// }
+// else{
+//     console.log('vag tui mokbul')
+// }
+
+// 
+
+
+
+// complex condition ..........................
+
+
+if ((salary> 20000 && hasCar== true) || isBCS == true){
+   
+        console.log('esho baba kobul')
+    
+    }
+    else{
+        console.log('vag tui mokbul')
+    }
