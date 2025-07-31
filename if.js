a749f36f6470fa3bdd64f5606c704da165808776
unknown <ummayjannatsadia@gmail.com>
2025-07-31 17:57:31 +0600

    14.4

---
+++ if.js
@@ -0,0 +1,29 @@
+// structure 
+// if(condition){
+
+// }
+// if (3< 10){
+
+//     console.log('I have a smaller Value')
+// }
+// ...............................
+
+
+/*var biriyaniPrice = 370;
+
+if(biriyaniPrice<300){
+    console.log('ek packet den')
+}
+else{
+    console.log('khaoa ar hoilOna')
+}
+
+//..................................
+
+// if(condition){
+
+// }
+// else{
+
+// }
+
