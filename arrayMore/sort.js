5952f4ea9965bfd1fbf8d19e1fc84c6695684c78
unknown <ummayjannatsadia@gmail.com>
2025-08-07 20:35:56 +0600

    sort

--- arrayMore/sort.js
+++ arrayMore/sort.js
@@ -8,7 +8,16 @@ friends.sort();
 // console.log(friends)
 
 
-const ages=[1, 5,12,345, 46, 234, 67, 34]
+const ages=[1, 5, 12, 345, 46, 234, 67, 34]
 console.log(ages)
 ages.sort();
-console.log(ages);
+// console.log(ages);
+
+// aescending
+const sorted_ages = ages.sort(function(a, b) {return  a - b})
+// console.log(sorted_ages)
+
+
+// descending
+const sorted_ages_desc = ages.sort(function(a, b){return b - a })
+// console.log(sorted_ages_desc)
